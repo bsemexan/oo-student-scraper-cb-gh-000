221fc83a2c78432cda0dc004ab36bba77d4b606a
Berry  Semexan <bsemexan@gmail.com>
2018-02-25 18:54:59 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,16 +1,16 @@
 require 'open-uri'
 require 'pry'
+require 'nokogiri'
 
 class Scraper
 
   def self.scrape_index_page(index_url)
     kickstarter = Nokogiri::HTML(index_url)
-    
+
   end
 
   def self.scrape_profile_page(profile_url)
-    
+
   end
 
 end
-
