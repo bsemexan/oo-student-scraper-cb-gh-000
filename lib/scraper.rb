ff166381552d8cc8fbaf0785db0d1b66865200c3
Berry  Semexan <bsemexan@gmail.com>
2018-02-25 19:02:45 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -32,6 +32,7 @@ class Scraper
         student[:blog] = link
       end
     end
+    student[:profile_quote] = profile_page.css(".profile-quote").text if profile_page.css(".profile-quote")
     student
   end
 
